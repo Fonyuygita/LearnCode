0a3d6934666ae4ffe701b0273b796ccdb34bd395
Fonyuy Gita <FonyuyJudegita@gmail.com>
2024-05-13 09:48:36 -0700

    changeed in layout

--- src/app/(root)/(home)/page.tsx
+++ src/app/(root)/(home)/page.tsx
@@ -10,59 +10,7 @@ import { Metadata } from "next"
 export const revalidate = 900;
 
 
-export const metadata: Metadata = {
-  title: "Learn Code, Coding Resources",
-  description:
-    "Get inspired with tech stuffs, Articles, talks, inspirations",
-  keywords: [
-    "learncode",
-    "fonyuy",
-    "learn coding",
-    "code learn",
-    "seed bamenda",
-    "seed 50 days of code",
-    "learncoding",
-    "fomonyuy jude",
-    "fonyuy jude fomonyuy",
-    "nquami",
-    "tech tutor vercel",
-    "tech tutor",
-    "tech tutor seed",
-    "fonyuy gita"
-  ],
-  openGraph: {
-    url: "https://learncode.vercel.app",
-    type: "website",
-    title: "Learn Code | SEED",
-    description:"Get inspired with tech stuffs, Articles, talks, inspirations.",
-    images: [
-      {
-        url: "https://i.ibb.co/9wwVdrM/studio-c38ec3ff8b3bff1213aa0851ab611dca-fdvkyhpv.jpg",
-        width: 1200,
-        height: 630,
-        alt: "gita"
-      }
-    ]
-  },
-  twitter: {
-    card: "summary_large_image",
-    title: "Learn Code, Coding Resources",
-    description:"Get inspired with tech stuffs, Articles, talks, inspirations",
-    creator: "@learncode",
-    site: "@learncode",
-    images: [
-      {
-        url: "https://i.ibb.co/9wwVdrM/studio-c38ec3ff8b3bff1213aa0851ab611dca-fdvkyhpv.jpg",
-        width: 1200,
-        height: 630,
-        alt: "fonyuygita"
-      }
-    ]
-  },
-  alternates: {
-    canonical: "https://learncode.vercel.app"
-  }
-};
+
 
 
 
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,9 +7,57 @@ const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
   title: 'Learn Code',
-  description: 'Sit down, take a rest and lear.',
+  description: ' Get inspired with tech stuffs, Articles, talks, inspirations, Sit down, take a rest and lear. ',
   manifest:'/manifest.json',
-  themeColor:'#0D1117'
+  themeColor:'#0D1117',
+  keywords: [
+    "learncode",
+    "fonyuy",
+    "learn coding",
+    "code learn",
+    "seed bamenda",
+    "seed 50 days of code",
+    "learncoding",
+    "fomonyuy jude",
+    "fonyuy jude fomonyuy",
+    "nquami",
+    "tech tutor vercel",
+    "tech tutor",
+    "tech tutor seed",
+    "fonyuy gita"
+  ],
+  openGraph: {
+    url: "https://learncode.vercel.app",
+    type: "website",
+    title: "Learn Code | SEED",
+    description:"Get inspired with tech stuffs, Articles, talks, inspirations.",
+    images: [
+      {
+        url: "https://i.ibb.co/9wwVdrM/studio-c38ec3ff8b3bff1213aa0851ab611dca-fdvkyhpv.jpg",
+        width: 1200,
+        height: 630,
+        alt: "gita"
+      }
+    ]
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Learn Code, Coding Resources",
+    description:"Get inspired with tech stuffs, Articles, talks, inspirations",
+    creator: "@learncode",
+    site: "@learncode",
+    images: [
+      {
+        url: "https://i.ibb.co/9wwVdrM/studio-c38ec3ff8b3bff1213aa0851ab611dca-fdvkyhpv.jpg",
+        width: 1200,
+        height: 630,
+        alt: "fonyuygita"
+      }
+    ]
+  },
+  alternates: {
+    canonical: "https://learncode.vercel.app"
+  }
   
 
 }
